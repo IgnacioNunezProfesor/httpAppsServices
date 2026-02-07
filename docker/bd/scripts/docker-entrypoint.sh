1b45d06e0f6ec58bd2d70e94e205e7d91f7c9147
Coder Nacho <profesor.inr@outlook.es>
2026-02-07 11:11:28 +0100

    entrypoint mejorado

--- docker/bd/scripts/docker-entrypoint.sh
+++ docker/bd/scripts/docker-entrypoint.sh
@@ -3,44 +3,42 @@ set -euo pipefail
 
 echo "[Entrypoint] Iniciando contenedor MariaDB"
 
-# Validación de variables obligatorias
-: "${DB_UNIX_USER:?Falta DB_UNIX_USER (usuario SQL)}"
+: "${DB_UNIX_USER:?Falta DB_UNIX_USER}"
 : "${DB_ROOT_PASS:?Falta DB_ROOT_PASS}"
 : "${DB_SERVER_DATA_DIR:?Falta DB_SERVER_DATA_DIR}"
+: "${DB_SERVER_LOG:=/var/log/mysql}"
+: "${DB_PORT:=3306}"
 
-# Inicializar datadir si está vacío
-if [ ! -d "${DB_SERVER_DATA_DIR}/mysql" ]; then
-    echo "[Entrypoint] Inicializando base de datos..."
-    mariadb-install-db --datadir="${DB_SERVER_DATA_DIR}"
+echo "[Entrypoint] Creating ${DB_UNIX_USER}"
+if ! id "${DB_UNIX_USER}" &>/dev/null; then
+    addgroup -S "${DB_UNIX_USER}"
+    adduser -S "${DB_UNIX_USER}" -G "${DB_UNIX_USER}"
 fi
+
+mkdir -p /run/mysqld
+chown ${DB_UNIX_USER}:${DB_UNIX_USER} /run/mysqld
+chmod 777 /run/mysqld
+
+mkdir -p /entrypointsql "${DB_SERVER_DATA_DIR}" "${DB_SERVER_LOG}"
+#chown -R "${DB_UNIX_USER}:${DB_UNIX_USER}" "${DB_SERVER_DATA_DIR}" "${DB_SERVER_LOG}" /entrypointsql
+
+# Inicializar base de datos
+if [ ! -d "${DB_SERVER_DATA_DIR}/mysql" ]; then
+    echo "[Entrypoint] Inicializando base de datos..." 
+    if ! output=$(mariadb-install-db --datadir="${DB_SERVER_DATA_DIR}" --user="${DB_UNIX_USER}" --basedir=/usr --auth-root-authentication-method=normal 2>&1); then 
+        echo "[Entrypoint] ERROR: mariadb-install-db falló" 
+        echo "[Entrypoint] Detalle del error:" 
+        echo "$output" 
+        exit 1 
+    fi 
+    echo "[Entrypoint] Base de datos inicializada correctamente"
+
+    echo "[Entrypoint] Aplicando configuración inicial (bootstrap)..."
+fi
+
 
 echo "[Entrypoint] Arrancando MariaDB..."
-mariadbd --datadir="${DB_SERVER_DATA_DIR}" & PID=$!
-
-# Esperar a que MariaDB esté listo
-echo "[Entrypoint] Esperando a MariaDB..."
-until mariadb-admin ping --silent; do
-    sleep 1
-done
-
-echo "[Entrypoint] Configurando usuarios SQL y base de datos..."
-mariadb -u root <<EOF
-ALTER USER 'root'@'%' IDENTIFIED BY '${DB_ROOT_PASS}';
-EOF
-
-# Ejecutar scripts SQL adicionales
-if [ -d "/entrypointsql" ]; then
-    for f in /entrypointsql/*.sql; do
-        [ -f "$f" ] || continue
-        echo "[Entrypoint] Ejecutando script: $f"
-        mariadb -u root -p"${DB_ROOT_PASS}" < "$f"
-    done
-fi
-
-echo "[Entrypoint] MariaDB listo."
 exec mariadbd \
     --datadir="${DB_SERVER_DATA_DIR}" \
-    --user="${DB_UNIX_USER}" \
     --bind-address=0.0.0.0 \
     --port="${DB_PORT}"
-wait "$PID"
