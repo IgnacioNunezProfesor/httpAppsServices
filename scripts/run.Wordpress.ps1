0e07b4e892eea2314e38db2e1ccfe3815749cdf3
Coder Nacho <profesor.inr@outlook.es>
2026-02-08 20:17:59 +0100

    TODO: New revolution, ps functions and libs

--- scripts/run.Wordpress.ps1
+++ scripts/run.Wordpress.ps1
@@ -1,24 +1,38 @@
 Param(
     [string]$EnvFile = ".\env\dev.wordpress.env"
 )
-$envVars = @{}
 
-if (-not (Test-Path $envFile)) {
-    Write-Error "Env file '$envFile' not found."
-    exit 1
-} 
-Get-Content $envFile | ForEach-Object {
-    if ($_ -match '^\s*([^=]+)=(.*)$') {
-        $envVars[$matches[1]] = $matches[2]
+function Get-EnvVarsFromFile {
+    param(
+        [Parameter(Mandatory)]
+        [string]$envFile
+    )
+
+    if (-not (Test-Path $envFile)) {
+        Write-Error "Env file '$envFile' not found."
+        exit 1
     }
+
+    $envVars = @{}
+
+    Get-Content $envFile | ForEach-Object {
+        if ($_ -match '^\s*([^=]+)=(.*)$') {
+            $envVars[$matches[1]] = $matches[2]
+        }
+    }
+
+    return $envVars
 }
+
+$envVars = Get-EnvVarsFromFile -envFile $EnvFile
+
 
 # Comprobar si existe la imagen phpapache:dev
 $image = docker images -q $envVars['IMAGE_NAME']
 
 if (-not $image) {
     Write-Host "La imagen $( $envVars['IMAGE_NAME'] ) NO existe. Lanzando build..."
-    ./scripts/build.phpApache.ps1 $envFile "./docker/phpapache.dev.dockerfile" $($envVars['IMAGE_NAME'])    
+    ./scripts/build.phpApache.ps1 $envFile $($envVars['IMAGE_NAME'])  $($envVars['IMAGE_NAME'])    
 } 
 
 $image = docker images -q $envVars['IMAGE_NAME']
