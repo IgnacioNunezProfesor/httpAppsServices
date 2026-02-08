ebd918bcda33c9b775f8f0c8b6e6739dde7032a6
Coder Nacho <profesor.inr@outlook.es>
2026-02-08 17:08:35 +0100

    Incluyendo el script que lance wordpress

---
+++ scripts/run.Wordpress.ps1
@@ -0,0 +1,29 @@
+Param(
+    [string]$EnvFile = ".\env\dev.wordpress.env"
+)
+$envVars = @{}
+
+if (-not (Test-Path $envFile)) {
+    Write-Error "Env file '$envFile' not found."
+    exit 1
+} 
+Get-Content $envFile | ForEach-Object {
+    if ($_ -match '^\s*([^=]+)=(.*)$') {
+        $envVars[$matches[1]] = $matches[2]
+    }
+}
+
+# Comprobar si existe la imagen phpapache:dev
+$image = docker images -q $envVars['IMAGE_NAME']
+
+if (-not $image) {
+    Write-Host "La imagen $( $envVars['IMAGE_NAME'] ) NO existe. Lanzando build..."
+    ./scripts/build.phpApache.ps1 $envFile "docker/phpapachephp.dev.dockerfile" $($envVars['IMAGE_NAME'])    
+} 
+
+$image = docker images -q $envVars['IMAGE_NAME']
+
+if ($image) {
+    Write-Host "La imagen $envVars['IMAGE_NAME'] existe. Lanzando run..."
+    ./run.phpapache.ps1 --EnvFile $envFile
+}
