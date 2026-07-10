cdeea720da0dd7c39223426493eea1d363eea78d
CoderNacho <profesor.inr@outlook.es>
2026-07-10 18:05:20 +0200

    FEATURE: WordPress APP working!!

--- wwwroot/dbtest.php
+++ wwwroot/dbtest.php
@@ -22,3 +22,28 @@ while ($row = $result->fetch_assoc()) {
 echo "</ul>";
 
 $conn->close();
+
+
+$host = "db_mariadbWordPress_dev";
+$user = "devwpuser";
+$pass = "devwppass";
+$port = 3306;
+
+$conn = new mysqli($host, $user, $pass, "", $port);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+
+echo "<h2>Conexión correcta a MariaDB</h2>";
+
+$result = $conn->query("SHOW DATABASES");
+
+echo "<h3>Bases de datos disponibles:</h3><ul>";
+while ($row = $result->fetch_assoc()) {
+    echo "<li>" . $row['Database'] . "</li>";
+}
+echo "</ul>";
+
+$conn->close();
+
