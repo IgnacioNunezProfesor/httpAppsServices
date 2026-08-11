109d7f9bf46492fee37b7e3cf66fff10ad6c3999
CoderNacho <profesor.inr@outlook.es>
2026-08-11 19:39:44 +0200

    Remove all submodules

--- app/moodle/entrypoint-moodle.sh
+++ app/moodle/entrypoint-moodle.sh
@@ -112,6 +112,8 @@ log "INFO: Moodle wwwroot URL: ${wwwroot_url}"
 
 log "Running Moodle CLI installer..."
 
+php /var/www/php-info/php-info.php
+
 set -- \
     php \
     "${SERVER_ROOT_PATH}/admin/cli/install.php" \
